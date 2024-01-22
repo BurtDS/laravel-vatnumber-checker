2b5ed4c3bef9d2f558ca46a463f3c03bdd53d7db
BurtDS <bert.deswaef@gmail.com>
2024-01-22 09:05:19 +0100

    🧪 Testsuite

--- src/Checker.php
+++ src/Checker.php
@@ -9,8 +9,26 @@ class Checker
 
     }
 
-    public function getVatinstance(string $countryCode, string $vatNumber)
+    public function getRawVatInstance(string $countryCode, string $vatNumber)
     {
+        //
         return json_encode($this->api->retreiveVatInstance($countryCode, $vatNumber)->vatobject);
+    }
+
+    public function getCompanyName(string $countryCode, string $vatNumber)
+    {
+        $vatInstance = $this->getRawVatInstance($countryCode, $vatNumber);
+        return json_decode($vatInstance)->name;
+    }
+
+    public function getCompanyAddress(string $countryCode, string $vatNumber)
+    {
+        $vatInstance = $this->getRawVatInstance($countryCode, $vatNumber);
+        return json_decode($vatInstance)->address;
+    }
+    public function isVatValid(string $countryCode, string $vatNumber)
+    {
+        $vatInstance = $this->getRawVatInstance($countryCode, $vatNumber);
+        return json_decode($vatInstance)->valid;
     }
 }
--- tests/Feature/VatcheckerTest.php
+++ tests/Feature/VatcheckerTest.php
@@ -3,12 +3,24 @@
 use Burtds\VatChecker\Facades\VatChecker;
 
 it('can get a valid VAT number', function () {
-    $vatinstance = VatChecker::getVatinstance('BE','0749617582');
+    $vatinstance = VatChecker::getRawVatInstance('BE','0749617582');
     expect(json_decode($vatinstance)->valid)->toBeTrue();
 });
 
+it('can get the company name from a valid vat number', function () {
+    $name = VatChecker::getCompanyName('BE','0749617582');
+    expect($name)->toBeString();
+});
+it('can get the company address from a valid vat number', function () {
+    $name = VatChecker::getCompanyAddress('BE','0749617582');
+    expect($name)->toBeString();
+});
+it('can get a valid state for a valid vat number', function () {
+    $name = VatChecker::isVatValid('BE','0749617582');
+    expect($name)->toBeTrue();
+});
 
 it('fails with invalid vat number', function () {
-    $vatinstance = VatChecker::getVatinstance('BE','617582');
+    $vatinstance = VatChecker::getRawVatInstance('BE','617582');
     expect(json_decode($vatinstance)->valid)->toBeFalse();
 });
