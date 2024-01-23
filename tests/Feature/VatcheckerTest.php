85bcce11231c123d6bc88adbdddaa596f5b33520
Freek Van der Herten <freek@spatie.be>
2024-01-23 19:58:18 +0100

    make tests breathe

--- tests/Feature/VatcheckerTest.php
+++ tests/Feature/VatcheckerTest.php
@@ -5,26 +5,30 @@ use Burtds\VatChecker\Facades\VatChecker;
 
 it('can get a valid VAT number', function () {
     $vatInstance = VatChecker::getRawVatInstance('BE', '0749617582');
+
     expect(json_decode($vatInstance)->valid)->toBeTrue();
 });
 
 it('can get the company name from a valid vat number', function () {
     $name = VatChecker::getCompanyName('BE', '0749617582'); // Test with Vulpo's VAT Number
+
     expect($name)->toBeString();
 });
 
 it('can get the company address from a valid vat number', function () {
     $address = VatChecker::getCompanyAddress('BE', '0749617582'); // Test with Vulpo's VAT Number
+
     expect($address)->toBeString();
 });
 
 it('can get a valid state for a valid vat number', function () {
     $isValid = VatChecker::isVatValid('BE', '0749617582'); // Test with Vulpo's VAT Number
+
     expect($isValid)->toBeTrue();
 });
 
 it('fails with invalid vat number', function () {
-    $vatinstance = VatChecker::getRawVatInstance('BE', '617582');
-    // expect(json_decode($vatinstance)->valid)->toBeFalse();
+    $vatInstance = VatChecker::getRawVatInstance('BE', '617582');
 
+    // expect(json_decode($vatInstance)->valid)->toBeFalse();
 })->throws(VatNumberNotFound::class);
