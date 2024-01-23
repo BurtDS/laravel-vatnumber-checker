9b83be65bc1d1c68c5ca4014f7bfe5eea6a953c6
Freek Van der Herten <freek@spatie.be>
2024-01-23 19:59:45 +0100

    avoid duplicating magic number

--- tests/Feature/VatcheckerTest.php
+++ tests/Feature/VatcheckerTest.php
@@ -3,26 +3,30 @@
 use Burtds\VatChecker\Exceptions\VatNumberNotFound;
 use Burtds\VatChecker\Facades\VatChecker;
 
+beforeEach(function () {
+    $this->validVatNumber = '0749617582'; // Vulpo's VAT Number
+});
+
 it('can get a valid VAT number', function () {
-    $vatInstance = VatChecker::getRawVatInstance('BE', '0749617582');
+    $vatInstance = VatChecker::getRawVatInstance('BE', $this->validVatNumber);
 
     expect(json_decode($vatInstance)->valid)->toBeTrue();
 });
 
 it('can get the company name from a valid vat number', function () {
-    $name = VatChecker::getCompanyName('BE', '0749617582'); // Test with Vulpo's VAT Number
+    $name = VatChecker::getCompanyName('BE', $this->validVatNumber);
 
     expect($name)->toBeString();
 });
 
 it('can get the company address from a valid vat number', function () {
-    $address = VatChecker::getCompanyAddress('BE', '0749617582'); // Test with Vulpo's VAT Number
+    $address = VatChecker::getCompanyAddress('BE', $this->validVatNumber);
 
     expect($address)->toBeString();
 });
 
 it('can get a valid state for a valid vat number', function () {
-    $isValid = VatChecker::isVatValid('BE', '0749617582'); // Test with Vulpo's VAT Number
+    $isValid = VatChecker::isVatValid('BE', $this->validVatNumber);
 
     expect($isValid)->toBeTrue();
 });
