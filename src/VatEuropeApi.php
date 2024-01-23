c7c374dd0640ba37319af3639b14b8449652cd50
Freek Van der Herten <freek@spatie.be>
2024-01-23 19:54:37 +0100

    remove unneeded comment

--- src/VatEuropeApi.php
+++ src/VatEuropeApi.php
@@ -12,7 +12,6 @@ class VatEuropeApi
 
     public function retrieveVatInstance(string $countryCode, string $vatNumber): VatInstance
     {
-        // $url = "{$this->api}/check-vat-number";
         $url = 'https://ec.europa.eu/taxation_customs/vies/rest-api/check-vat-number';
 
         $response = Http::post($url, [
