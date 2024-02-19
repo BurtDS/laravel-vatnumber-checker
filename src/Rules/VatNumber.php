332fcb64d809e6c5cf1766265b0f2ae020cafe5d
BurtDS <bert.deswaef@gmail.com>
2024-02-19 23:31:48 +0100

    🔨

---
+++ src/Rules/VatNumber.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Burtds\VatChecker\Rules;
+
+use Burtds\VatChecker\Facades\VatChecker;
+use Illuminate\Contracts\Validation\Rule;
+
+class VatNumber implements Rule
+{
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $vatInstance = VatChecker::getRawVatInstance($value);
+        return json_decode($vatInstance)->valid;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The :attribute is not valid.';
+    }
+}
